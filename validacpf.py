56a762fd25b636b5fbc444c44b49bc3ac439ff8b
Leonardo Castro <leocasttro01@icloud.com>
2022-01-17 10:41:51 -0300

    Validador de CPF

---
+++ validacpf.py
@@ -0,0 +1,33 @@
+# Loop infinito
+while True:
+    # cpf = '16899535009'
+    cpf = input('Digite um CPF: ')
+    novo_cpf = cpf[:-2]                 
+    reverso = 10                        
+    total = 0
+
+    # Loop do CPF
+    for index in range(19):
+        if index > 8:                   
+            index -= 9                  
+
+        total += int(novo_cpf[index]) * reverso  # Valor total da multiplicação
+
+        reverso -= 1                    
+        if reverso < 2:
+            reverso = 11
+            d = 11 - (total % 11)
+
+            if d > 9:                   
+                d = 0
+            total = 0                   
+            novo_cpf += str(d)          
+
+    # Evita sequencias. Ex.: 11111111111, 00000000000...
+    sequencia = novo_cpf == str(novo_cpf[0]) * len(cpf)
+
+    
+    if cpf == novo_cpf and not sequencia:
+        print('Válido')
+    else:
+        print('Inválido')
